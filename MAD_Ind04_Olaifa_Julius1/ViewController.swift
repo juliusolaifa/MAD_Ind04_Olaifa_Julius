a67a1903ac128bc40e4adae30fe521d304bc53c1
Julius Olaifa <julius.olaifa@okstate.edu>
2022-04-05 20:17:11 -0500

    Initial Commit

---
+++ MAD_Ind04_Olaifa_Julius1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD_Ind04_Olaifa_Julius1
+//
+//  Created by Julius Olaifa on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
